de792da91164201fb8c55ce72ce8e8f0fa8f7079
syed <arshadsyed074@gmail.com>
2025-11-11 14:40:15 +0530

    cors fix

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ const myorders=require('./db/User/myorders')
 const app = express();      
 app.use(express.json());
 app.use(cors({
-    origin: ["https://medly-pharma.onrender.com"],
+    origin: ["http://localhost:3000"],
     methods: ["POST", "GET", "DELETE", "PUT"],
     credentials: true
 }));
